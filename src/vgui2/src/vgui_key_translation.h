f09fe9dcefa72323c2ebb9bfa9f97ee308e24714
evil-inject0r <none-of-your@busienss.com>
2019-04-28 00:49:10 +0200

    reverted to original files

--- src/vgui2/src/vgui_key_translation.h
+++ src/vgui2/src/vgui_key_translation.h
@@ -1,4 +1,4 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
+//===== Copyright © 1996-2005, Valve Corporation, All rights reserved. ======//
 //
 // Purpose: 
 //
@@ -10,7 +10,7 @@
 #pragma once
 #endif
 
-#include <vgui/KeyCode.h>
+#include <vgui/keycode.h>
 
 // Convert from Windows scan codes to VGUI key codes.
 vgui::KeyCode KeyCode_VirtualKeyToVGUI( int key );
