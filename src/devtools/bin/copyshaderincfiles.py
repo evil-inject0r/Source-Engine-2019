d670a38e17dc593faea0d6381d90b1d52193c08a
evil-inject0r <none-of-your@busienss.com>
2019-07-31 23:58:16 +0200

    minor changes to shader python script

--- src/devtools/bin/copyshaderincfiles.py
+++ src/devtools/bin/copyshaderincfiles.py
@@ -11,25 +11,21 @@ from shutil import copyfile
 dirpath = os.getcwd()
 file_name = open(sys.argv[1], "r")
 
-def FileExists(file):
+def file_exists(file):
     my_file = Path(file)
     return my_file.exists()
-
-def CleanFileName(filename):
-    return filename.replace("\n", "")
 
 for file in file_name:
     
     if file.startswith('//') or file.startswith('\n'):
         continue
-    
-    src_file = CleanFileName(file)
-    src_file = dirpath + "\\" + src_file
-    
-    dst_file = CleanFileName(file).replace("_tmp", "")
-    dst_file = dirpath + "\\" + dst_file
 
-    if FileExists(src_file):
+    file = file.replace("\n", "")
+    
+    src_file = dirpath + "\\" + file
+    dst_file = dirpath + "\\" + file.replace("_tmp", "")
+
+    if file_exists(src_file):
         copyfile(src_file, dst_file)
     else:
         print(src_file + " does not exist")
